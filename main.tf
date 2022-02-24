5f7016a7343e1514849ce7d65c980a6c18386339
NgumNfor <ngumjuden1234@gmail.com>
2022-02-23 20:44:31 -0500

    added

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
 
 
-
-
 module "vpcmodule" {
   source = "git::git@github.com:NGUMNFOR/FirstTerraformproject.git//vpcmodule"
 
-  vpccidr        = "10.0.0.0/16"
+  vpccidr        = "200.0.0.0/16"
   WebsubnetNames = 3
-  websubnet_cidr = [for each in range(0, 225, 2) : cidrsubnet("10.0.0.0/16", 8, each)]
-  appsubnet_cidr = [for each in range(1, 225, 2) : cidrsubnet("10.0.0.0/16", 8, each)]
+  websubnet_cidr = [for each in range(0, 225, 2) : cidrsubnet("200.0.0.0/16", 8, each)]
+  appsubnet_cidr = [for each in range(1, 225, 2) : cidrsubnet("200.0.0.0/16", 8, each)]
   AppsubnetNames = 3 
 }
